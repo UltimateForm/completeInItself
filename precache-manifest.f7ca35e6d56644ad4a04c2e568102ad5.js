638b970b36ea5f418791c86fad69b74f14f76c63
helio <heliojmsemedo@gmail.com>
2020-10-03 22:34:32 +0100

    Updates

---
+++ precache-manifest.f7ca35e6d56644ad4a04c2e568102ad5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee835e8c1eaaf11bca64029de4e1a56",
+    "url": "/completeinitself/index.html"
+  },
+  {
+    "revision": "1e9fb5fd9b8fb6749bc3",
+    "url": "/completeinitself/static/css/main.69ba6320.chunk.css"
+  },
+  {
+    "revision": "32e37dc204a5c836f156",
+    "url": "/completeinitself/static/js/2.d7c8ff85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/completeinitself/static/js/2.d7c8ff85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e9fb5fd9b8fb6749bc3",
+    "url": "/completeinitself/static/js/main.c13d588a.chunk.js"
+  },
+  {
+    "revision": "1d7e1bca004691d6f882",
+    "url": "/completeinitself/static/js/runtime-main.67052fe2.js"
+  },
+  {
+    "revision": "1d7c2c9d21ca88a85239fdfb03674295",
+    "url": "/completeinitself/static/media/1 riley.1d7c2c9d.jpg"
+  },
+  {
+    "revision": "28f822ad38a2ede594bbd09d390e638c",
+    "url": "/completeinitself/static/media/10 angels egg.28f822ad.jpg"
+  },
+  {
+    "revision": "7d19b17d9e1b8b7b6613c3e26d39c60a",
+    "url": "/completeinitself/static/media/11 Abysmal education.7d19b17d.jpg"
+  },
+  {
+    "revision": "29ec471f1eb3511231996f9b632943d0",
+    "url": "/completeinitself/static/media/12 Excessive sweetness.29ec471f.jpg"
+  },
+  {
+    "revision": "53dd861f1ffbd2452993af4e6714025b",
+    "url": "/completeinitself/static/media/13 Goodnight smooch.53dd861f.jpg"
+  },
+  {
+    "revision": "1dace5d6531adc03e7c1e27201728593",
+    "url": "/completeinitself/static/media/14 Money embezzlement.1dace5d6.jpg"
+  },
+  {
+    "revision": "1f428ac50fdbabae117cd650a191cf2a",
+    "url": "/completeinitself/static/media/2 flying whales.1f428ac5.jpg"
+  },
+  {
+    "revision": "2d5ce25a44ba8ff84e34591203b6ab71",
+    "url": "/completeinitself/static/media/3 prosvijetljena.2d5ce25a.jpg"
+  },
+  {
+    "revision": "559d31940401e9ed04bf0c458f35c2b5",
+    "url": "/completeinitself/static/media/4 the discovery.559d3194.jpg"
+  },
+  {
+    "revision": "ebcc6fa7f5305c8e3c8fef49611e8d45",
+    "url": "/completeinitself/static/media/6 portal.ebcc6fa7.jpg"
+  },
+  {
+    "revision": "0754d4fcecc71969838c21d3aaacdfd4",
+    "url": "/completeinitself/static/media/7 star.0754d4fc.jpg"
+  },
+  {
+    "revision": "5a0814f261823843e6734918399be574",
+    "url": "/completeinitself/static/media/8 ripples.5a0814f2.jpg"
+  },
+  {
+    "revision": "c51e9513cff4604720b375a372c0062a",
+    "url": "/completeinitself/static/media/9 eye.c51e9513.jpg"
+  },
+  {
+    "revision": "f3684598db8d92cf53167ad189e3ba60",
+    "url": "/completeinitself/static/media/background.f3684598.jpg"
+  },
+  {
+    "revision": "e945f57784de23e20a94dee18db2542b",
+    "url": "/completeinitself/static/media/composition-edited.e945f577.png"
+  }
+]);
